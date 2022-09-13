68dc95de0ce00a32a6642011799f10d26402ce6b
Morgan <morgan.espindola@gmail.com>
2022-09-13 08:32:51 -0300

    Class 01

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>;
+}
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello World" />
+    </>
+  );
+}
+
+export default App;
