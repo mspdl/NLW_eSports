d89879041c07f0b09b74609c966ab4fa909b1aeb
Morgan <morgan.espindola@gmail.com>
2022-09-14 19:50:08 -0300

    Class 03: server (backend)

---
+++ server/src/utils/convert-minutes-to-hour-string.ts
@@ -0,0 +1,9 @@
+export function convertMinutesToHourString(minutesAmount: number) {
+  const hours = Math.floor(minutesAmount / 60);
+  const minutes = minutesAmount % 60;
+
+  const hoursFormatted = String(hours).padStart(2, "0");
+  const minutesFormatted = String(minutes).padStart(2, "0");
+
+  return `${hoursFormatted}:${minutesFormatted}`;
+}
